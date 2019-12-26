00edd80757ef27ac491084ef1bac3c86e4213a5f
Maysa <hajajmaysa@gmail.com>
2019-12-26 13:43:54 +0200

    neww

---
+++ js/script-vue.js
@@ -0,0 +1,36 @@
+var vm = new Vue ({
+    el:'#sec-login',
+    data:{
+    imgsrc:'img/header/logo@2x.png'
+    }
+
+    })
+    var vm = new Vue ({
+        el:'#navbar',
+        data:{
+        home:'الرئيسية',
+        ahome:'index.html',
+        sbmc:'ملابس نسائية',
+        asbmc:'Clothes.html',
+        Experts:'رجالي',
+        aExperts:'Clothes.html',
+        section:'أحذية',
+        asection:'Clothes.html',
+        Designers:'حقائب',
+        aDesigners:'Clothes.html',
+        brand:'إكسسوارات',
+        abrand:'Clothes.html',
+        Theatrical:'ملابس للنوم',
+        aTheatrical:'Clothes.html',
+        hasri:'ملابس داخلية',
+        ahasri:'Clothes.html',
+        size:'مقاسات كبيره',
+        asize:'Clothes.html',
+        Tv:'رياضي',
+        aTV:'Clothes.html',
+        market:'الأم والطفل',
+        amarket:'Clothes.html'
+       
+        }
+    
+        })
